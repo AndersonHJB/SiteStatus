f3a07e51ea80dac7f493b3843b6d40934cc0a2a8
AndersonHJB <aiyuechuang@gmail.com>
2025-01-28 12:07:02 +0800

    更新健康检查脚本，重命名变量以提高可读性，优化日志保留策略，修正 HTTP 状态码检查逻辑

--- health-check.sh
+++ health-check.sh
@@ -9,7 +9,7 @@ commit=true
 # fi
 
 KEYSARRAY=()
-COMMAND_ARGS=()
+URLSARRAY=()
 
 urlsConfig="./urls.cfg"
 echo "Reading $urlsConfig"
@@ -17,8 +17,8 @@ while read -r line
 do
   echo "  $line"
   IFS='=' read -ra TOKENS <<< "$line"
-  KEYSARRAY+=("${TOKENS[0]}")
-  COMMAND_ARGS+=("$(eval echo "${TOKENS[1]}")")
+  KEYSARRAY+=(${TOKENS[0]})
+  URLSARRAY+=(${TOKENS[1]})
 done < "$urlsConfig"
 
 echo "***********************"
@@ -29,15 +29,13 @@ mkdir -p logs
 for (( index=0; index < ${#KEYSARRAY[@]}; index++))
 do
   key="${KEYSARRAY[index]}"
-  args="$(echo ${COMMAND_ARGS[index]})"
+  url="${URLSARRAY[index]}"
+  echo "  $key=$url"
 
-  echo "  $key=$args"
-  
   for i in 1 2 3 4; 
   do
-    cmd="curl --write-out '%{http_code}' --silent --output /dev/null $args"
-    response=$(eval "$cmd" | tee)
-    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 307 ]; then
+    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
+    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
       result="success"
     else
       result="failed"
@@ -51,8 +49,8 @@ do
   if [[ $commit == true ]]
   then
     echo $dateTime, $result >> "logs/${key}_report.log"
-    # By default we keep 10000 last log entries.  Feel free to modify this to meet your needs.
-    echo "$(tail -10000 logs/${key}_report.log)" > "logs/${key}_report.log"
+    # By default we keep 2000 last log entries.  Feel free to modify this to meet your needs.
+    echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
   else
     echo "    $dateTime, $result"
   fi
@@ -61,8 +59,8 @@ done
 if [[ $commit == true ]]
 then
   # Let's make Vijaye the most productive person on GitHub.
-  git config --global user.name 'AndersonHJB'
-  git config --global user.email 'aiyuechuang@gmail.com'
+  git config --global user.name 'Vijaye Raji'
+  git config --global user.email 'vijaye@statsig.com'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
